fc378c4752bedda20c5b4caecd31061f2a80b9ba
Yun-SeYeong <dbstpdud09@naver.com>
2021-06-25 00:25:39 +0900

    [UPDATE] update profile page, dashboard to stack

--- lib/pages/DashBoardPage.dart
+++ lib/pages/DashBoardPage.dart
@@ -12,195 +12,223 @@ class DashBoardPage extends StatefulWidget {
 
 class _DashBoardPageState extends State<DashBoardPage> {
   final PageController pageController = PageController(
-    initialPage: 0
+      initialPage: 0
   );
+
   @override
   Widget build(BuildContext context) {
-    final double _statusBarHeight = MediaQuery.of(context).padding.top;
+    final double _statusBarHeight = MediaQuery
+        .of(context)
+        .padding
+        .top;
     return Scaffold(
-      body: PageView(
-        controller: pageController,
+      body: Stack(
         children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
+          PageView(
+            controller: pageController,
             children: [
-              Container(
-                height: 48.0,
-                margin:
-                EdgeInsets.only(top: _statusBarHeight, left: 32.0, right: 32.0),
-                child: Stack(
-                  children: [
-                    Center(
-                      child: const Text(
-                        "BAKING",
-                        style: TextStyle(fontSize: 18),
-                      ),
-                    ),
-                    IconButton(
-                      onPressed: () {},
-                      icon: const Icon(Icons.more_vert),
-                    )
-                  ],
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.only(left: 32.0, right: 32.0, top: 36.0),
-                child: Column(
-                  children: [
-                    ElevatedButton(
-                      onPressed: () {},
-                      style: ButtonStyle(backgroundColor:
-                      MaterialStateProperty.resolveWith((states) {
-                        return Colors.white;
-                      })),
-                      child: Container(
-                        height: 48.0,
-                        child: Row(
-                          children: [
-                            const Icon(
-                              Icons.search,
-                              color: Color(0xff8f8f8f),
-                            ),
-                            Container(width: 4.0),
-                            Text(
-                              "Search...",
-                              style: const TextStyle(
-                                color: Color(0xff8f8f8f),
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-                    Container(
-                      margin: const EdgeInsets.only(top: 36.0),
-                      child: BestRecipesWidget(recipeList: [
-                        Recipe("name1"),
-                        Recipe("name2"),
-                        Recipe("name3"),
-                      ]),
-                    ),
-                    Container(
-                      margin: const EdgeInsets.only(top: 36.0),
-                      child: Column(
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              Text(
-                                "Categories",
-                                style: TextStyle(fontSize: 24.0),
-                              ),
-                            ],
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    height: 48.0,
+                    margin:
+                    EdgeInsets.only(
+                        top: _statusBarHeight, left: 32.0, right: 32.0),
+                    child: Stack(
+                      children: [
+                        Center(
+                          child: const Text(
+                            "BAKING",
+                            style: TextStyle(fontSize: 18),
                           ),
-                          Container(
-                            padding: EdgeInsets.only(top: 24.0),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(Icons.more_vert),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(
+                        left: 32.0, right: 32.0, top: 36.0),
+                    child: Column(
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {},
+                          style: ButtonStyle(backgroundColor:
+                          MaterialStateProperty.resolveWith((states) {
+                            return Colors.white;
+                          })),
+                          child: Container(
+                            height: 48.0,
                             child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                               children: [
-                                ElevatedButton(
-                                  onPressed: () {},
-                                  style: ButtonStyle(
-                                    backgroundColor:
-                                    MaterialStateProperty.resolveWith((states) {
-                                      return Colors.white;
-                                    }),
-                                    padding: MaterialStateProperty.resolveWith(
-                                            (states) => EdgeInsets.all(4.0)),
-                                  ),
-                                  child: Container(
-                                    height: 48.0,
-                                    width: 48.0,
-                                    child: Image.asset("img/cookie.png"),
-                                  ),
+                                const Icon(
+                                  Icons.search,
+                                  color: Color(0xff8f8f8f),
                                 ),
-                                ElevatedButton(
-                                  onPressed: () {},
-                                  style: ButtonStyle(
-                                    backgroundColor:
-                                    MaterialStateProperty.resolveWith((states) {
-                                      return Colors.white;
-                                    }),
-                                    padding: MaterialStateProperty.resolveWith(
-                                            (states) => EdgeInsets.all(4.0)),
+                                Container(width: 4.0),
+                                Text(
+                                  "Search...",
+                                  style: const TextStyle(
+                                    color: Color(0xff8f8f8f),
                                   ),
-                                  child: Container(
-                                    height: 48.0,
-                                    width: 48.0,
-                                    child: Image.asset("img/bread.png"),
-                                  ),
-                                ),
-                                ElevatedButton(
-                                  onPressed: () {},
-                                  style: ButtonStyle(
-                                    backgroundColor:
-                                    MaterialStateProperty.resolveWith((states) {
-                                      return Colors.white;
-                                    }),
-                                    padding: MaterialStateProperty.resolveWith(
-                                            (states) => EdgeInsets.all(4.0)),
-                                  ),
-                                  child: Container(
-                                    height: 48.0,
-                                    width: 48.0,
-                                    child: Image.asset("img/birthday-cake.png"),
-                                  ),
-                                ),
-                                ElevatedButton(
-                                  onPressed: () {},
-                                  style: ButtonStyle(
-                                    backgroundColor:
-                                    MaterialStateProperty.resolveWith((states) {
-                                      return Colors.white;
-                                    }),
-                                    padding: MaterialStateProperty.resolveWith(
-                                            (states) => EdgeInsets.all(4.0)),
-                                  ),
-                                  child: Container(
-                                    height: 48.0,
-                                    width: 48.0,
-                                    child: Image.asset("img/pie.png"),
-                                  ),
-                                ),
+                                )
                               ],
                             ),
-                          )
-                        ],
-                      ),
-                    )
-                  ],
-                ),
+                          ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.only(top: 36.0),
+                          child: BestRecipesWidget(recipeList: [
+                            Recipe("name1"),
+                            Recipe("name2"),
+                            Recipe("name3"),
+                          ]),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.only(top: 36.0),
+                          child: Column(
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "Categories",
+                                    style: TextStyle(fontSize: 24.0),
+                                  ),
+                                ],
+                              ),
+                              Container(
+                                padding: EdgeInsets.only(top: 24.0),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment
+                                      .spaceEvenly,
+                                  children: [
+                                    ElevatedButton(
+                                      onPressed: () {},
+                                      style: ButtonStyle(
+                                        backgroundColor:
+                                        MaterialStateProperty.resolveWith((
+                                            states) {
+                                          return Colors.white;
+                                        }),
+                                        padding: MaterialStateProperty
+                                            .resolveWith(
+                                                (states) =>
+                                                EdgeInsets.all(4.0)),
+                                      ),
+                                      child: Container(
+                                        height: 48.0,
+                                        width: 48.0,
+                                        child: Image.asset("img/cookie.png"),
+                                      ),
+                                    ),
+                                    ElevatedButton(
+                                      onPressed: () {},
+                                      style: ButtonStyle(
+                                        backgroundColor:
+                                        MaterialStateProperty.resolveWith((
+                                            states) {
+                                          return Colors.white;
+                                        }),
+                                        padding: MaterialStateProperty
+                                            .resolveWith(
+                                                (states) =>
+                                                EdgeInsets.all(4.0)),
+                                      ),
+                                      child: Container(
+                                        height: 48.0,
+                                        width: 48.0,
+                                        child: Image.asset("img/bread.png"),
+                                      ),
+                                    ),
+                                    ElevatedButton(
+                                      onPressed: () {},
+                                      style: ButtonStyle(
+                                        backgroundColor:
+                                        MaterialStateProperty.resolveWith((
+                                            states) {
+                                          return Colors.white;
+                                        }),
+                                        padding: MaterialStateProperty
+                                            .resolveWith(
+                                                (states) =>
+                                                EdgeInsets.all(4.0)),
+                                      ),
+                                      child: Container(
+                                        height: 48.0,
+                                        width: 48.0,
+                                        child: Image.asset(
+                                            "img/birthday-cake.png"),
+                                      ),
+                                    ),
+                                    ElevatedButton(
+                                      onPressed: () {},
+                                      style: ButtonStyle(
+                                        backgroundColor:
+                                        MaterialStateProperty.resolveWith((
+                                            states) {
+                                          return Colors.white;
+                                        }),
+                                        padding: MaterialStateProperty
+                                            .resolveWith(
+                                                (states) =>
+                                                EdgeInsets.all(4.0)),
+                                      ),
+                                      child: Container(
+                                        height: 48.0,
+                                        width: 48.0,
+                                        child: Image.asset("img/pie.png"),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
               ),
+              Center(
+                child: Text("page2"),
+              ),
+              Center(
+                child: Text("page3"),
+              ),
+              ProfilePage(),
             ],
           ),
-          Center(
-            child: Text("page2"),
-          ),
-          Center(
-            child: Text("page3"),
-          ),
-          ProfilePage(),
+          Positioned(child: BottomNavigationWidget(onTap: (index) {
+            final pageCurve = Curves.fastOutSlowIn;
+            final pageDuration = const Duration(milliseconds: 300);
+            print("index $index");
+            switch (index) {
+              case 0:
+                pageController.animateToPage(
+                    0, duration: pageDuration, curve: pageCurve);
+                break;
+              case 1:
+                pageController.animateToPage(
+                    1, duration: pageDuration, curve: pageCurve);
+                break;
+              case 2:
+                pageController.animateToPage(
+                    2, duration: pageDuration, curve: pageCurve);
+                break;
+              case 3:
+                pageController.animateToPage(
+                    3, duration: pageDuration, curve: pageCurve);
+                break;
+            }
+          }), bottom: 0.0,)
         ],
       ),
-      bottomNavigationBar: BottomNavigationWidget(onTap: (index) {
-        final pageCurve = Curves.fastOutSlowIn;
-        final pageDuration = const Duration(milliseconds: 300);
-        print("index $index");
-        switch(index) {
-          case 0:
-            pageController.animateToPage(0, duration: pageDuration, curve: pageCurve);
-            break;
-          case 1:
-            pageController.animateToPage(1, duration: pageDuration, curve: pageCurve);
-            break;
-          case 2:
-            pageController.animateToPage(2, duration: pageDuration, curve: pageCurve);
-            break;
-          case 3:
-            pageController.animateToPage(3, duration: pageDuration, curve: pageCurve);
-            break;
-        }
-      }),
     );
   }
 }
--- lib/pages/ProfilePage.dart
+++ lib/pages/ProfilePage.dart
@@ -14,13 +14,16 @@ class ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     final double _statusBarHeight = MediaQuery.of(context).padding.top;
+    final double _height = MediaQuery.of(context).size.height;
+    final double _titleHeight = 48.0;
+    final double _contentHeight = _height - _statusBarHeight - _titleHeight;
     return Scaffold(
       body: Column(
         children: [
           Container(
-            height: 48.0,
+            height: _titleHeight,
             margin:
-            EdgeInsets.only(top: _statusBarHeight, left: 32.0, right: 32.0),
+                EdgeInsets.only(top: _statusBarHeight, left: 32.0, right: 32.0),
             child: Stack(
               children: [
                 Center(
@@ -37,20 +40,29 @@ class ProfilePageState extends State<ProfilePage> {
             ),
           ),
           Container(
+            height: _contentHeight,
             padding: const EdgeInsets.symmetric(horizontal: 40.0),
-            child: Column(
+            child: ListView(
               children: [
-                SizedBox(height: 18.0,),
+                SizedBox(
+                  height: 18.0,
+                ),
                 MyProfileWidget(),
                 SizedBox(
                   height: 28.0,
                 ),
-                MyRecipesWidget(recipes: [
-                  Recipe(
-                      "madlen",
-                      "프랑스가 원산지인 과자로, 카스텔라와 비슷한 맛이 난다. 기본적으로는 조개 모양의 틀로 구워서 사진과 같이 조개 모양으로 굽지만, 사용한 모양틀에 따라 다",
-                      "549")
-                ])
+                MyRecipesWidget(
+                  recipes: [
+                    Recipe(
+                        "madlen",
+                        "프랑스가 원산지인 과자로, 카스텔라와 비슷한 맛이 난다. 기본적으로는 조개 모양의 틀로 구워서 사진과 같이 조개 모양으로 굽지만, 사용한 모양틀에 따라 다",
+                        "549"),
+                    Recipe(
+                        "madlen",
+                        "프랑스가 원산지인 과자로, 카스텔라와 비슷한 맛이 난다. 기본적으로는 조개 모양의 틀로 구워서 사진과 같이 조개 모양으로 굽지만, 사용한 모양틀에 따라 다",
+                        "549")
+                  ],
+                )
               ],
             ),
           ),
